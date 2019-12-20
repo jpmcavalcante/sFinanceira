a0ce329848df99abd1a833f1c97dd4c9815e5868
João Pedro <43858399+jpmcavalcante@users.noreply.github.com>
2019-12-20 09:20:48 -0300

    Ajuste de JS e CSS
    -PAG Template

--- Views/template.php
+++ Views/template.php
@@ -10,22 +10,20 @@
     <!-- Font Awesome Icons -->
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/adminlte/plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
-    <!--<link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/adminlte/dist/css/adminlte.min.css">-->
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/adminlte/dist/css/adminlte.min.css">
     <!-- Theme style nova proposta-->
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/novaProposta/NovaPasta.css">
     <!-- Google Font: Source Sans Pro -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-    <!--CSS Interno-->
+
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/style.css">
-    <!--Bootastrap 4.4.1-->
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 </head>
-<body class="hold-transition sidebar-mini" style="background: #ebebeb; box-sizing: border-box;">
-<div class="wrapper" style="width: 100vw;">
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
 
     <!-- Navbar -->
-    <nav class="navbar navbar-light container-fluid" style="">
-    <div class="container-fluid">
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light bar-header" style="">
+    <div class="container">
     <!-- Left navbar links -->
         <ul class="navbar-nav botao-menu">
             <li class="nav-item">
@@ -52,15 +50,12 @@
     <!-- /.navbar -->
 
     <!-- Main Sidebar Container -->
-    <aside class="main-sidebar sidebar-style elevation-4 bg-info">
+    <aside class="main-sidebar sidebar-style elevation-4">
         <!-- Brand Logo -->
         <div class="brand-logo">
-            <img src="<?php echo BASE_URL; ?>assets/img/logo_ms.png" alt="Logo Sistema MS" class="brand-image"
+            <img src="<?php echo BASE_URL; ?>assets/img/logo_ms.png" alt="Logo Leite Neves Advocacia" class="brand-image"
                  style="max-width: 80%;">
         </div>
-
-
-
         <!-- Sidebar -->
         <div class="sidebar list-sidebar" >
             <!-- Sidebar user panel (optional) -->
@@ -158,7 +153,7 @@
     <!-- /.control-sidebar -->
 
     <!-- Main Footer -->
-    <footer class="main-footer container-fluid" style="text-align: center;">
+    <footer class="main-footer" style="text-align: center">
         <!-- To the right -->
         
         <!-- Default to the left -->
@@ -172,9 +167,7 @@
 <!-- jQuery -->
 <script src="<?php echo BASE_URL; ?>assets/adminlte/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="<?php echo BASE_URL; ?>assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="<?php echo BASE_URL; ?>assets/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-
 <!-- AdminLTE App -->
 <script src="<?php echo BASE_URL; ?>assets/adminlte/dist/js/adminlte.min.js"></script>
 <!-- jQuery mask min-->
