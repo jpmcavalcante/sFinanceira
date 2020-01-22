d063a7d11e45a837c96fd61d32968e749846bf61
João Pedro <43858399+jpmcavalcante@users.noreply.github.com>
2020-01-22 15:46:17 -0300

    Pág Proposta
    -1 Etapa

--- Views/novaProposta.php
+++ Views/novaProposta.php
@@ -16,7 +16,7 @@
 
 
 <div class="row mb-2 cd-flex justify-content-center">
-    <div class="container-fluid">
+    <div class="container-fluid container p-3" style="background: #ccc; width: 85vw; border-radius: 1%;">
     
         <ul class="nav nav-tabs">
             <li class="nav-item">
@@ -24,32 +24,33 @@
             </li>
         </ul>
         
-        <div style="margin: 40px"></div>
+        
         <div class="tab-content">
 
             <!-- progress bar -->
-            <div class="progress">
-            <div class="progress-bar bg-success" role="progressbar" id="progress-bar" style="width: 25%; font-size: 16px " aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">1/4</div>
-            </div>  
+            <div class="progress mt-3">
+                <span class="progress-bar bg-success" role="progressbar" id="progress-bar" style="width: 25%; font-size: 16px " aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">1/4</div>
+                </span>  
 
-            <nav style="margin-top: 20px">
-            <div class="nav nav-tabs" id="nav-tab" role="tablist">
-                <a style="width: 25%" class="nav-item nav-link active" id="nav-dadosDaOperacao-tab" data-toggle="tab" href="#nav-dadosDaOperacao" role="tab" aria-controls="nav-home" aria-selected="true">1. DADOS DA OPERAÇÃO</a>
-                <a style="width: 25%" class="nav-item nav-link" id="nav-dadosPessoais-tab" data-toggle="tab" href="#nav-dadosPessoais" role="tab" aria-controls="nav-dadosPessoais" aria-selected="false">2. DADOS PESSOAIS</a>
-                <a style="width: 25%" class="nav-item nav-link" id="nav-dadosBancarios-tab" data-toggle="tab" href="#nav-dadosBancarios" role="tab" aria-controls="nav-contact" aria-selected="false">3. DADOS BANCARIOS</a>
-                <a style="width: 25%" class="nav-item nav-link" id="nav-anexos-tab" data-toggle="tab" href="#nav-anexos" role="tab" aria-controls="nav-anexos" aria-selected="false">4. ANEXOS</a>
-            </div>
-            </nav>
+                <nav class="mt-4">
+                    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                        <a style="width: 25%" class="nav-item nav-link active" id="nav-dadosDaOperacao-tab" data-toggle="tab" href="#nav-dadosDaOperacao" role="tab" aria-controls="nav-home" aria-selected="true">1. DADOS DA OPERAÇÃO</a>
+                        <a style="width: 25%" class="nav-item nav-link" id="nav-dadosPessoais-tab" data-toggle="tab" href="#nav-dadosPessoais" role="tab" aria-controls="nav-dadosPessoais" aria-selected="false">2. DADOS PESSOAIS</a>
+                        <a style="width: 25%" class="nav-item nav-link" id="nav-dadosBancarios-tab" data-toggle="tab" href="#nav-dadosBancarios" role="tab" aria-controls="nav-contact" aria-selected="false">3. DADOS BANCARIOS</a>
+                        <a style="width: 25%" class="nav-item nav-link" id="nav-anexos-tab" data-toggle="tab" href="#nav-anexos" role="tab" aria-controls="nav-anexos" aria-selected="false">4. ANEXOS</a>
+                    </div>
+                </nav>
 
             <form id="etapa1" name="etapa1" action="<?php BASE_URL;?>Proposta/add_action" method="post" enctype="multipart/form-data">
                 <div class="tab-content" id="nav-tabContent">
                 <div class="tab-pane fade show active" id="nav-dadosDaOperacao" role="tabpanel" aria-labelledby="nav-dadosDaOperacao-tab">
 
-                <div >
+                <div class="mt-4">
                     <input type="hidden" name="nome_colaborador" value="<?php $viewData['colId']->getName(); ?>">
+                    
                     <div >
                         <div class="col-md-4">
-                            <select id="operacao" name="operacao" class="form-control">
+                            <select class="form-control mt-2" id="operacao" name="operacao" class="form-control">
                                 <option value="" disabled selected>selecione um tipo de operação</option>
                                 <option value="Cartão de Credito">Cartão de Credito</option>
                             </select>
@@ -76,128 +77,136 @@
                         </div>
 
                     </div>
-                    <hr>
+                    <hr style="background: #474747; height: 5px;">
                     <div style="margin: 20px; font-weight: bold;">
                         <span id="content" ></span>
                     </div>
                     <hr>
+                    
+                    <!--INICIO 1ª ETAPA-->
                     <div id="div-etapa1" hidden>
-                        <div >
-                            <div >
-                                <div class="col-md-4">
-                                    <label for="valor" class="col-sm-3 col-form-label">Valor R$</label>
-                                    <div >
-                                        <input type="text" class="form-control valor" id="valor" name="valor" >
+
+                            <!--VALOR E PARCELAS-->
+                                <div class="">
+                                    <div class="input-group col-md-4">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">R$</span>
+                                        </div>
+                                        <div >
+                                            <input type="text" class="form-control valor" id="valor" name="valor" placeholder="Digite o Valor">
+                                        </div>
+                                        <span id="valor" hidden></span>
                                     </div>
-                                    <span id="valor" hidden></span>
+                                    <div class="input-group col-md-4">
+                                        <select id="QtParcelas" name="QtParcelas" class="form-control-sm">
+                                            <option value="" disabled selected>Parcelas</option>
+                                        </select>
+                                        <span id="QtParcelas" hidden></span>
+                                        <input type="text" id="valorParcela" name="valorParcela" hidden>
+                                    </div> 
+                                    <span class="input-group col-md-2" id="total" style="margin-top: 5px; font-weight: bold;  color: red;"></span>
+                            
+                                    <input style="display: none;" id="valorFinal" type="text" name="valorFinal" readonly>
                                 </div>
-                                <div class="col-md-4">
-                                    <select id="QtParcelas" name="QtParcelas" class="form-control">
-                                        <option value="" disabled selected>Qtde de Parcelas</option>
-                                    </select>
-                                    <span id="QtParcelas" hidden></span>
-                                    <input type="text" id="valorParcela" name="valorParcela" hidden>
-                                </div> 
+                            
+                            <!--FIM VALOR E PARCELAS-->
+                            <!-- VALOR TOTAL -->
+                            
+                            <hr style="background: #474747; height: 5px;">
+                            <!-- BANDEIARA -->
+                            <div class="col-md-4 mb-1">
+                                <select id="bandeiraBancaria" name="bandeiraBancaria" class="form-control">
+                                    <option value="" disabled selected>Bandeira do Cartão</option>
+                                    <option value="Elo" >Elo</option>
+                                    <option value="Visa" >Visa</option>
+                                    <option value="Amex" >Amex</option>
+                                    <option value="HiperCard" >HiperCard</option>
+                                    <option value="MasterCard" >MasterCard</option>
+                                </select>
+                                <span id="bandeiraBancaria" hidden></span>
                             </div>
-                        </div>
-                        <hr>
-                        <!-- Valor total -->
-                        <div  id="total" style="margin: 5px; font-weight: bold;"></div>
-                        <input  id="valorFinal" type="text" name="valorFinal" readonly>
-                        <!-- BANDEIARA -->
-                        <div class="col-md-4">
-                            <select id="bandeiraBancaria" name="bandeiraBancaria" class="form-control">
-                                <option value="" disabled selected>Bandeira do Cartão</option>
-                                <option value="Elo" >Elo</option>
-                                <option value="Visa" >Visa</option>
-                                <option value="Amex" >Amex</option>
-                                <option value="HiperCard" >HiperCard</option>
-                                <option value="MasterCard" >MasterCard</option>
-                            </select>
-                            <span id="bandeiraBancaria" hidden></span>
-                        </div>
-                        <!-- Numero do cartão -->
-                        <div class="col-md-4">
-                            <label for="numeroCartao" class="col-sm-4 col-form-label">Nº do Cartão</label>
-                            <div >
-                                <input type="text" class="form-control" id="numeroCartao" name="numeroCartao" placeholder="">
+                            <!-- Numero do cartão -->
+                            <div class="col-md-4 mb-1">
+                                <div >
+                                    <input type="text" class="form-control" id="numeroCartao" name="numeroCartao" placeholder="Nº do Cartão">
+                                </div>
+                                <span id="numeroCartao" hidden></span>
                             </div>
-                            <span id="numeroCartao" hidden></span>
-                        </div>
-                        <!-- Titular do cartão -->
-                        <div class="col-md-4">
-                            <label for="titular" class="col-sm-4 col-form-label">Titular do Cartão</label>
-                            <div >
-                                <input type="text" class="form-control" id="titular" name="titular" placeholder="">
+                            <!-- Titular do cartão -->
+                            <div class="col-md-4 mb-1">
+                                <div >
+                                    <input type="text" class="form-control" id="titular" name="titular" placeholder="Nome Impresso no Cartão">
+                                </div>
+                                <span id="titular" hidden></span>
                             </div>
-                            <span id="titular" hidden></span>
-                        </div>
-                        <!-- Mês Vencimento -->
-                        <div class="col-md-4">
-                            <select id="mesVenci" name="mesVenci" class="form-control">
-                                <option value="" disabled selected>Mês venci...</option>
-                                <option value="Janeiro">Janeiro</option>
-                                <option value="Fevereiro">Fevereiro</option>
-                                <option value="Março">Março</option>
-                                <option value="Abril">Abril</option>
-                                <option value="Maio">Maio</option>
-                                <option value="Junho">Junho</option>
-                                <option value="Julho">Julho</option>
-                                <option value="Agosto">Agosto</option>
-                                <option value="Setembro">Setembro</option>
-                                <option value="Outubro">Outubro</option>
-                                <option value="Novembro">Novembro</option>
-                                <option value="Dezembro">Dezembro</option>
-                            </select>
-                            <span id="mesVenci" hidden></span>
-                        </div>
-                        <!-- Ano vencimento -->
-                        <div class="col-md-4">
-                            <select id="anoVenci" name="anoVenci" class="form-control">
-                                <option value="" disabled selected>Ano venci...</option>
-                                <option value="2020">2020</option>
-                                <option value="2021">2021</option>
-                                <option value="2022">2022</option>
-                                <option value="2023">2023</option>
-                                <option value="2024">2024</option>
-                                <option value="2025">2025</option>
-                                <option value="2026">2026</option>
-                                <option value="2027">2027</option>
-                                <option value="2028">2028</option>
-                                <option value="2029">2029</option>
-                            </select>
-                            <span id="anoVenci" hidden></span>
-                        </div>
-                        <!-- codigo Sg -->
-                        <div class="col-md-4">
-                            <label for="codigoSeguranca" class="col-sm-6 col-form-label">Codigo .Segurança</label>
-                            <div >
-                                <input type="text" class="form-control" id="codigoSeguranca" name="codigoSeguranca" placeholder="">
+                            <!-- Mês Vencimento -->
+                            <div class="col-md-4 mb-1">
+                                <select id="mesVenci" name="mesVenci" class="form-control">
+                                    <option value="" disabled selected>Mês Vencimento</option>
+                                    <option value="Janeiro">Janeiro</option>
+                                    <option value="Fevereiro">Fevereiro</option>
+                                    <option value="Março">Março</option>
+                                    <option value="Abril">Abril</option>
+                                    <option value="Maio">Maio</option>
+                                    <option value="Junho">Junho</option>
+                                    <option value="Julho">Julho</option>
+                                    <option value="Agosto">Agosto</option>
+                                    <option value="Setembro">Setembro</option>
+                                    <option value="Outubro">Outubro</option>
+                                    <option value="Novembro">Novembro</option>
+                                    <option value="Dezembro">Dezembro</option>
+                                </select>
+                                <span id="mesVenci" hidden></span>
                             </div>
-                            <span id="codigoSeguranca" hidden></span>
-                        </div>
+                            <!-- Ano vencimento -->
+                            <div class="col-md-4 mb-1">
+                                <select id="anoVenci" name="anoVenci" class="form-control">
+                                    <option value="" disabled selected>Ano Vencimento</option>
+                                    <option value="2020">2020</option>
+                                    <option value="2021">2021</option>
+                                    <option value="2022">2022</option>
+                                    <option value="2023">2023</option>
+                                    <option value="2024">2024</option>
+                                    <option value="2025">2025</option>
+                                    <option value="2026">2026</option>
+                                    <option value="2027">2027</option>
+                                    <option value="2028">2028</option>
+                                    <option value="2029">2029</option>
+                                </select>
+                                <span id="anoVenci" hidden></span>
+                            </div>
+                            <!-- codigo Sg -->
+                            <div class="col-md-4">
+                                <label for="codigoSeguranca" class="col-sm-6 col-form-label">Codigo de Segurança</label>
+                                <div >
+                                    <input type="text" class="form-control" id="codigoSeguranca" name="codigoSeguranca" placeholder="">
+                                </div>
+                                <span id="codigoSeguranca" hidden></span>
+                            </div>
                         </div>
 
+                        <button id="dadosP" class="btn btn-info next mt-2" type="button" >Continue</button>  
                         
-                        <button id="dadosP" class="btn btn-primary next" type="button" >Continue</button>   
                     </div>
+                    <!--FIM 1ª ETAPA-->
 
                 </div>
+
+            <!--INÍCIO 2ª ETAPA-->
             <div class="tab-pane fade" id="nav-dadosPessoais" role="tabpanel" aria-labelledby="nav-dadosPessoais-tab">
-                <div class="row">
+                <div class="row mt-3">
                     
                     <div class="col-md-12 ">
-                        <label>Informe o nome do cliente</label>
-                        <input type="text" name="textoBusca" id="textoBusca" >
+                        <label>Nome do Cliente</label>
+                        <input class="form-control-sm" type="text" name="textoBusca" id="textoBusca" >
                         <span id="textoBusca" hidden></span>
                     </div>
-                    <div class="col-md-12 " id="resultado_busca" >
-
-                    </div>
                     
+                    <span style="color: red;" class="col-md-12 " id="resultado_busca"></span>
+                           
                     <div class="col-md-12 " style="margin-top: 30px;">
 
-                        <label for="nome">id</label>
+                        <label for="nome">ID</label>
                         <input type="text" name="idCli" id="id" disabled>
 
                         <label for="nome" >Nome</label>
